4f6ecb1e65411edbc219395cdcd146ea34ff2cbc
The-uncanny-valley <runcannyvalley@gmail.com>
2026-07-25 21:17:07 +0300

    feat(tester): add ResultStatusBadge and configure Compose
    Enable Compose in the tester module and add necessary dependencies. Implement a ResultStatusBadge component to display success or failure states using new vector icons.

--- feature/tester/build.gradle.kts
+++ feature/tester/build.gradle.kts
@@ -25,9 +25,18 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
---
+++ feature/tester/src/main/java/android/reagent/tester/components/ResultStatusBadge.kt
@@ -0,0 +1,53 @@
+package android.reagent.tester.components
+
+import android.reagent.designsystem.NeonGreen
+import android.reagent.designsystem.Vermilion
+import android.reagent.tester.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ResultStatusBadge(
+    success: Boolean,
+) {
+    Box(
+        modifier = Modifier
+            .size(24.dp)
+            .clip(CircleShape)
+            .background(
+                if (success) {
+                    NeonGreen
+                } else {
+                    Vermilion
+                }
+            )
+    ) {
+        val icon = if (success) {
+            painterResource(R.drawable.ic_tick)
+        } else {
+            painterResource(R.drawable.ic_cross)
+        }
+
+        Icon(
+            modifier = Modifier.align(Alignment.Center),
+            painter = icon,
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ResultStatusBadgePreview() {
+    ResultStatusBadge(success = true)
+}
