59c71eb34fa44d173caf5bf99d2120418579aeff
The-uncanny-valley <runcannyvalley@gmail.com>
2026-07-25 22:11:11 +0300

    feat: create button

---
+++ feature/tester/src/main/java/android/reagent/tester/components/RunRequestButton.kt
@@ -0,0 +1,81 @@
+package android.reagent.tester.components
+
+import android.reagent.data.R
+import android.reagent.designsystem.NeonYellow
+import android.reagent.designsystem.ReagentTheme
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RunRequestButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean,
+    onClick: () -> Unit,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.95f else 1f,
+        animationSpec = tween(durationMillis = 120),
+        label = "button_scale"
+    )
+
+    Button(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(25.dp))
+            .height(56.dp)
+            .scale(scale)
+            .background(NeonYellow),
+
+        enabled = enabled,
+
+        onClick = onClick,
+
+        shape = RoundedCornerShape(16.dp)
+    ) {
+
+        Icon(
+            painter = painterResource(android.reagent.tester.R.drawable.ic_play),
+            contentDescription = null
+        )
+
+        Spacer(
+            Modifier.width(8.dp)
+        )
+
+        Text("Run Request")
+    }
+}
+
+@Preview
+@Composable
+private fun RunRequestButton_Preview() {
+    ReagentTheme {
+        RunRequestButton(
+            enabled = true,
+            onClick = {}
+        )
+    }
+}
