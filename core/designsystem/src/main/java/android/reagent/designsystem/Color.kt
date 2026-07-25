c82133075042413a8a143409bdb00c83e99c9578
The-uncanny-valley <runcannyvalley@gmail.com>
2026-07-25 20:43:29 +0300

    feat(designsystem): add Archivo font family and define new color constants

--- core/designsystem/src/main/java/android/reagent/designsystem/Color.kt
+++ core/designsystem/src/main/java/android/reagent/designsystem/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Nero = Color(0xFF161616)
+val Platinum = Color(0xFFE2E2E2)
+val NeonYellow = Color(0xFFD0EF31)
+val NeonGreen = Color(0xFF48FF00)
+val Vermilion = Color(0xFFFF4D00)
--- core/designsystem/src/main/java/android/reagent/designsystem/Type.kt
+++ core/designsystem/src/main/java/android/reagent/designsystem/Type.kt
@@ -2,9 +2,14 @@ package android.reagent.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+
+val Archivo = FontFamily(
+    Font(R.font.archivo_black_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,15 +19,16 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Archivo,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
-    ),
+    )
+    /* Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
