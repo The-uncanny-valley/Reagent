1a4553a5770b3f4ea94a98a813bd4293c01fa2a3
The-uncanny-valley <runcannyvalley@gmail.com>
2026-08-15 18:54:12 +0300

    fix: align recent endpoints to start

--- feature/tester/src/main/java/android/reagent/tester/components/RecentEndpointsRow.kt
+++ feature/tester/src/main/java/android/reagent/tester/components/RecentEndpointsRow.kt
@@ -17,16 +17,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import android.reagent.designsystem.ReagentTheme
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ChipColors
+import androidx.compose.ui.Alignment
 
 @Composable
 fun RecentEndpointsRow(
     endpoints: List<String>,
     onRunRecentRequest: (String) -> Unit
 ) {
-    Column {
-
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
         Text(
             text = "Recent",
             style = MaterialTheme.typography.labelMedium,
@@ -38,6 +42,7 @@ fun RecentEndpointsRow(
         )
 
         LazyRow(
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
